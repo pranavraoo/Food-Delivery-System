f24ebcd772304eaddeb4c08d0690e3baf0107e28
pranavraoo <pranavraop2023@gmail.com>
2025-12-24 11:27:50 +0530

    hopefully, this is it

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log('MongoDB Atlas connected');
+    console.log('Connected to DB:', mongoose.connection.name);
   } catch (error) {
     console.error('MongoDB connection failed:', error);
     process.exit(1);
   }
+  
 };
